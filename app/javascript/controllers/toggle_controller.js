dcfca2445bfa65a8372e517a8718d6e352d576bd
Patricio <patriciopherrero@gmail.com>
2024-02-28 17:54:44 +0100

    redone

---
+++ app/javascript/controllers/toggle_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="toggle"
+export default class extends Controller {
+  static targets = ["toggleElement"]
+
+  fire() {
+    this.toggleElementTarget.classList.toggle("d-none")
+  }
+}
